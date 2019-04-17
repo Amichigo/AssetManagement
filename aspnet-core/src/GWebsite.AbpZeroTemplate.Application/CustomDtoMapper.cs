02b6d876c3f96238a5ea673a8a236e2d60e1fc9d
HUY CAO MINH <16520502@ms.uit.edu.vn>
2019-04-17 09:55:28 +0700

    finish API

--- aspnet-core/src/GWebsite.AbpZeroTemplate.Application/CustomDtoMapper.cs
+++ aspnet-core/src/GWebsite.AbpZeroTemplate.Application/CustomDtoMapper.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using GWebsite.AbpZeroTemplate.Application.Share.Customers.Dto;
 using GWebsite.AbpZeroTemplate.Application.Share.DemoModels.Dto;
 using GWebsite.AbpZeroTemplate.Application.Share.MenuClients.Dto;
+using GWebsite.AbpZeroTemplate.Application.Share.ShoppingPlans.Dto;
 using GWebsite.AbpZeroTemplate.Core.Models;
 
 namespace GWebsite.AbpZeroTemplate.Applications
@@ -26,6 +27,12 @@ namespace GWebsite.AbpZeroTemplate.Applications
             configuration.CreateMap<CustomerInput, Customer>();
             configuration.CreateMap<Customer, CustomerInput>();
             configuration.CreateMap<Customer, CustomerForViewDto>();
+
+            //ShoppingPlan
+            configuration.CreateMap<ShoppingPlan, ShoppingPlanDto>();
+            configuration.CreateMap<ShoppingPlanInput, ShoppingPlan>();
+            configuration.CreateMap<ShoppingPlan, ShoppingPlanInput>();
+            configuration.CreateMap<ShoppingPlan, ShoppingPlanForViewDto>();
         }
     }
 }
